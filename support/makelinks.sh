93e014abde3ad674e966f5b66ba513a7f9246ca2
moomoo <moomoo@moomooi.local>
2013-08-13 13:40:52 +0900

    Add global gitconfig, gitignore

--- support/makelinks.sh
+++ support/makelinks.sh
@@ -1,10 +1,12 @@
-#!/usr/local/bin/zsh
-cd ~
-ln -sf dotfiles/vimify/editrc .editrc
-ln -sf dotfiles/vimify/inputrc .inputrc
-ln -sf dotfiles/tmux/tmux.conf .tmux.conf
-ln -sf dotfiles/vim .vim
-ln -sf dotfiles/vim/vimrc .vimrc
-ln -sf dotfiles/zsh/prezto .zprezto
-ln -sf dotfiles/zsh .zsh
-ln -sf .zsh/zshrc .zshrc
+#!/bin/sh
+cd ${HOME}
+ln -sfn dotfiles/vimify/editrc .editrc
+ln -sfn dotfiles/vimify/inputrc .inputrc
+ln -sfn dotfiles/tmux/tmux.conf .tmux.conf
+ln -sfn dotfiles/vim .vim
+ln -sfn dotfiles/vim/vimrc .vimrc
+ln -sfn dotfiles/zsh/prezto .zprezto
+ln -sfn dotfiles/zsh .zsh
+ln -sfn .zsh/zshrc .zshrc
+ln -sfn dotfiles/git/gitconfig .gitconfig
+ln -sfn dotfiles/git/gitignore .gitignore
