1c57beac98c333642dbfc32bb52caac85fdb76c0
Seonhyu Kim <seonhyu@iroisoft.com>
2026-01-11 12:59:14 +0900

    feat: tmux active pane 강조 설정 추가
    - tmux active pane 배경색을 catppuccin-macchiato bg(#24273a)로 설정
    - nvim 투명 배경 활성화로 tmux pane 배경색 표시

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -4,7 +4,11 @@ return {
   name = 'catppuccin',
   lazy = false,
   priority = 1000,
-  config = function()
+  opts = {
+    transparent_background = true,
+  },
+  config = function(_, opts)
+    require('catppuccin').setup(opts)
     vim.cmd.colorscheme('catppuccin-macchiato')
   end,
 }
