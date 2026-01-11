043ac45b07ef6385ae37f2841a8fa305f4951a28
Seonhyu Kim <seonhyu@iroisoft.com>
2026-01-11 10:06:18 +0900

    fix: Telescope 파일 미리보기 설정 추가
    - layout_config로 preview 영역 설정 (55% 너비)
    - preview_cutoff 80 컬럼으로 표시 조건 설정
    - treesitter preview 활성화
    - ui.lua 코드 포맷팅 수정 (탭→스페이스)
    - virt-column 플러그인 제거

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,56 +1,68 @@
 -- Telescope - Fuzzy Finder
 return {
-  'nvim-telescope/telescope.nvim',
-  event = 'VimEnter',
+  "nvim-telescope/telescope.nvim",
+  event = "VimEnter",
   dependencies = {
-    'nvim-lua/plenary.nvim',
+    "nvim-lua/plenary.nvim",
     {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'make',
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
       cond = function()
-        return vim.fn.executable('make') == 1
+        return vim.fn.executable("make") == 1
       end,
     },
-    { 'nvim-telescope/telescope-ui-select.nvim' },
-    { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+    { "nvim-telescope/telescope-ui-select.nvim" },
+    { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
   },
   config = function()
-    require('telescope').setup({
+    require("telescope").setup({
       defaults = {
         file_ignore_patterns = {
-          'deps/',
-          '_build/',
-          'node_modules/',
+          "deps/",
+          "_build/",
+          "node_modules/",
+        },
+        layout_strategy = "horizontal",
+        layout_config = {
+          horizontal = {
+            preview_width = 0.45,
+            preview_cutoff = 140,
+          },
+          width = 0.87,
+          height = 0.80,
+        },
+        preview = {
+          treesitter = true,
         },
       },
       extensions = {
-        ['ui-select'] = {
-          require('telescope.themes').get_dropdown(),
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown(),
         },
       },
     })
 
-    pcall(require('telescope').load_extension, 'fzf')
-    pcall(require('telescope').load_extension, 'ui-select')
+    pcall(require("telescope").load_extension, "fzf")
+    pcall(require("telescope").load_extension, "ui-select")
 
-    local builtin = require('telescope.builtin')
+    local builtin = require("telescope.builtin")
 
-    vim.keymap.set('n', '<leader>/', function()
-      builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
+    vim.keymap.set("n", "<leader>/", function()
+      builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
         winblend = 10,
         previewer = false,
       }))
-    end, { desc = '[/] Fuzzily search in current buffer' })
+    end, { desc = "[/] Fuzzily search in current buffer" })
 
-    vim.keymap.set('n', '<leader>s/', function()
+    vim.keymap.set("n", "<leader>s/", function()
       builtin.live_grep({
         grep_open_files = true,
-        prompt_title = 'Live Grep in Open Files',
+        prompt_title = "Live Grep in Open Files",
       })
-    end, { desc = '[S]earch [/] in Open Files' })
+    end, { desc = "[S]earch [/] in Open Files" })
 
-    vim.keymap.set('n', '<leader>sn', function()
-      builtin.find_files({ cwd = vim.fn.stdpath('config') })
-    end, { desc = '[S]earch [N]eovim files' })
+    vim.keymap.set("n", "<leader>sn", function()
+      builtin.find_files({ cwd = vim.fn.stdpath("config") })
+    end, { desc = "[S]earch [N]eovim files" })
   end,
 }
